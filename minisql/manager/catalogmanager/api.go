71643ad40f6da0ec0b68254aab46daef18c18004
lsw7777 <116267140+lsw7777@users.noreply.github.com>
2023-05-21 13:34:42 +0800

    Update api.go

--- minisql/manager/catalogmanager/api.go
+++ minisql/manager/catalogmanager/api.go
@@ -3,13 +3,15 @@ package catalogmanager
 import( 
 	index "Distributed-MiniSQL/minisql/manager/commonutil"
 )
-//DataType类
+
+//定义数据类型结构体类型
 type Datatype struct {
 	NumType int // 1为char,2 为int, 3为float
 	length  int //长度
 }
 
-func NewDatatype(NumType int, length int) *Datatype { //int和float
+//定义新的数据类型
+func NewDatatype(NumType int, length int) *Datatype { 、
 	if NumType == 1 {
 		return &Datatype{NumType: 1, length: length}
 	}
@@ -20,33 +22,34 @@ func NewDatatype(NumType int, length int) *Datatype { //int和float
 	}
 }
 
-// func NewDatatype2(NumType int, length int) *Datatype{ //varchar
-// 	return &Datatype{NumType: 1, length: length}
-// }
 
-//Attribute类
+
+//定义属性结构体类型
 type Attribute struct {
 	AttributeName string
 	FieldType     Datatype
 	IsUnique      bool //该属性是否是unique的
 }
 
+//定义新属性
 func NewAttribute(attributeName string, NumType int, length int, isUnique bool) *Attribute {
 	newDatatype := NewDatatype(NumType, length)
 	return &Attribute{AttributeName: attributeName, FieldType: *newDatatype, IsUnique: isUnique}
 }
 
-//Address类
+//定义地址结构体类型
 type Address struct {
 	FileName    string
 	BlockOffset int //file中的block offset
 	ByteOffset  int //block 中的
 }
 
+//定义新地址
 func NewAddress(fileName string, blockOffset int, byteOffset int) *Address {
 	return &Address{FileName: fileName, BlockOffset: blockOffset, ByteOffset: byteOffset}
 }
 
+//用于比较两个地址
 func (a Address) Compare(b Address) bool {
 	if a.FileName == b.FileName {
 		if a.BlockOffset == b.BlockOffset {
@@ -59,7 +62,7 @@ func (a Address) Compare(b Address) bool {
 	}
 }
 
-//Table类
+//定义表结构体类型
 type Table struct {
 	TableName       string
 	PrimaryKey      string
@@ -71,6 +74,7 @@ type Table struct {
 	RowLength       int
 }
 
+//定义新表
 func NewTable(tableName string, primaryKey string, attributeVector []Attribute) *Table {
 	var table = Table{TableName: tableName, PrimaryKey: primaryKey,
 		AttributeVector: attributeVector, AttributeNum: len(attributeVector)}
@@ -83,6 +87,7 @@ func NewTable(tableName string, primaryKey string, attributeVector []Attribute) 
 	return &table
 }
 
+//也是定义新表。新增两个参数：索引向量和行数。
 func NewTable2(tableName string, primaryKey string, attributeVector []Attribute, indexVector []index.Index, rowNum int) *Table {
 	var table = Table{TableName: tableName, PrimaryKey: primaryKey,
 		AttributeVector: attributeVector, AttributeNum: len(attributeVector), IndexVector: indexVector, IndexNum: len(indexVector), RowNum: rowNum}
