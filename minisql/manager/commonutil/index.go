21c2d207f5dca31e14226044cea14e661ad9d9c2
lsw7777 <116267140+lsw7777@users.noreply.github.com>
2023-05-21 14:45:04 +0800

    Update index.go

--- minisql/manager/commonutil/index.go
+++ minisql/manager/commonutil/index.go
@@ -1,6 +1,6 @@
 package index
 
-//index类
+//定义索引结构体类型
 type Index struct {
 	IndexName     string
 	TableName     string
@@ -10,10 +10,12 @@ type Index struct {
 	BlockNum int
 }
 
+//产生新索引
 func NewIndex(indexName string, tableName string, attributeName string) *Index {
 	return &Index{IndexName: indexName, TableName: tableName, AttributeName: attributeName}
 }
 
+//产生新索引
 func NewIndex2(indexName string, tableName string, attributeName string, blockNum int, rootNum int) *Index {
 	return &Index{IndexName: indexName, TableName: tableName, AttributeName: attributeName, BlockNum: blockNum, RootNum: rootNum}
 }
