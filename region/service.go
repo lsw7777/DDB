248a003316924e38d45195e4912c3e63ff08e437
lsw7777 <116267140+lsw7777@users.noreply.github.com>
2023-05-21 13:16:05 +0800

    Update service.go

--- region/service.go
+++ region/service.go
@@ -10,6 +10,8 @@ import (
 	"Distributed-MiniSQL/minisql/manager/interpreter"
 )
 
+
+//用于处理接收到的 SQL 语句，它会调用 region.processSQL 方法来将 SQL 语句转换为对 etcd 的操作，并返回执行结果。如果执行出错，则会返回错误信息
 func (region *Region) Process(input *string, reply *string) error {
 	log.Printf("Region.Process called: %v", *input)
 	res, err := region.processSQL(*input)
@@ -35,6 +37,7 @@ func (region *Region) Process(input *string, reply *string) error {
 	return err
 }
 
+//用于将当前节点设为备份节点，并连接到指定的备份节点
 func (region *Region) AssignBackup(ip *string, dummyReply *bool) error {
 	log.Printf("Region.AssignBackup called: backup ip: %v", *ip)
 	client, err := rpc.DialHTTP("tcp", *ip+REGION_PORT)
@@ -52,6 +55,7 @@ func (region *Region) AssignBackup(ip *string, dummyReply *bool) error {
 	return err
 }
 
+//用于将备份IP地址从当前Region结构体中移除，并关闭与备份客户端的连接。
 func (region *Region) RemoveBackup(dummyArgs, dummyReply *bool) error {
 	log.Printf("Region.RemoveBackup called: remove %v", region.backupIP)
 	region.backupIP = ""
@@ -62,6 +66,7 @@ func (region *Region) RemoveBackup(dummyArgs, dummyReply *bool) error {
 	return nil
 }
 
+//用于下载指定IP的快照，并清除Region结构体中的备份IP。
 func (region *Region) DownloadSnapshot(ip *string, dummyReply *bool) error {
 	log.Printf("Region.DownloadSnapshot called: download %v's snapshot", *ip)
 	region.RemoveBackup(nil, nil)
@@ -70,6 +75,7 @@ func (region *Region) DownloadSnapshot(ip *string, dummyReply *bool) error {
 	return nil
 }
 
+//用于将接收到的SQL查询语句作为参数，并将其解析和处理为结果字符串
 func (region *Region) processSQL(sql string) (string, error) {
 	res := interpreter.Interpret(sql)
 
