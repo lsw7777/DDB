6decc44055b833c6c70a2cc918a7727f1774833f
lsw7777 <116267140+lsw7777@users.noreply.github.com>
2023-05-21 12:34:28 +0800

    Update master.go

--- master/master.go
+++ master/master.go
@@ -12,23 +12,19 @@ import (
 	. "Distributed-MiniSQL/common"
 )
 
-// map values not addressable, yet everything in Go is passed by value
-// thus we need pointers to slices
-// check https://go.dev/play/p/rvqLX4XFgRK
 
+//定义Master结构体变量
 type Master struct {
 	regionCount int
-
 	etcdClient    *clientv3.Client
 	regionClients map[string]*rpc.Client
-
-	serverTables map[string]*[]string // ip->tables
-	tableIP      map[string]string    // table->ip
-	indexInfo    map[string]string    // index->table
-
+	serverTables map[string]*[]string 
+	tableIP      map[string]string   
+	indexInfo    map[string]string    
 	backupInfo map[string]string
 }
 
+//初始化并返回一个Master变量
 func (master *Master) Init(regionCount int) {
 	master.regionCount = regionCount
 
@@ -40,6 +36,7 @@ func (master *Master) Init(regionCount int) {
 	master.backupInfo = make(map[string]string)
 }
 
+//启动Master主节点对外提供RPC服务，不断等待新的请求的到来
 func (master *Master) Run() {
 	// connect to local etcd server
 	master.etcdClient, _ = clientv3.New(clientv3.Config{
@@ -59,17 +56,20 @@ func (master *Master) Run() {
 	}
 }
 
+//在Master服务器中增加一个表
 func (master *Master) addTable(table, ip string) {
 	master.tableIP[table] = ip
 	AddUniqueToSlice(master.serverTables[ip], table)
 }
 
+//在Master服务器中删除一个表
 func (master *Master) deleteTable(table, ip string) {
 	master.deleteTableIndices(table)
 	delete(master.tableIP, table)
 	DeleteFromSlice(master.serverTables[ip], table)
 }
 
+//删除 Master服务器中某个表的所有索引
 func (master *Master) deleteTableIndices(table string) {
 	targets := make([]string, 0)
 	for idx, tbl := range master.indexInfo {
@@ -82,6 +82,7 @@ func (master *Master) deleteTableIndices(table string) {
 	}
 }
 
+//选择当前可用且任务负载最轻的服务器，并返回其 IP 地址
 func (master *Master) bestServer() string {
 	min, res := math.MaxInt, ""
 	for ip, pTables := range master.serverTables {
@@ -92,6 +93,7 @@ func (master *Master) bestServer() string {
 	return res
 }
 
+//将一个服务器上的所有表格转移到另一个服务器，并在目的服务器中更新相关信息
 func (master *Master) transferServerTables(src, dst string) {
 	pTables := master.serverTables[src]
 	for _, table := range *pTables {
@@ -101,6 +103,7 @@ func (master *Master) transferServerTables(src, dst string) {
 	delete(master.serverTables, src)
 }
 
+//删除指定IP地址的服务器上的所有表
 func (master *Master) removeServerTables(ip string) {
 	pTables := master.serverTables[ip]
 	for _, table := range *pTables {
