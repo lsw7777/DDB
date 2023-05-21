9d0d58fd2a74daedf4d018ef48c37f12991ca958
lsw7777 <116267140+lsw7777@users.noreply.github.com>
2023-05-21 12:13:34 +0800

    Update common.go

--- common/common.go
+++ common/common.go
@@ -10,36 +10,41 @@ import (
 
 type Identity int
 
+//常量定义
 const (
+	//超时
 	TIMEOUT_S = 1000
 	TIMEOUT_M = 2000
 	TIMEOUT_L = 10000
 
-	// networking
+	//网络连接
 	NETWORK = "tcp"
 
 	MASTER_PORT = ":4733"
 	REGION_PORT = ":2016"
 
-	// etcd
+	//Etcd的主机地址
 	HOST_ADDR = "127.0.0.1:2379"
 
-	// minisql files
+	//工作区域
 	WORKING_DIR = "distributed-mini-sql/"
 	DIR         = "sql/"
 )
 
+//定义"表命令"结构体类型
 type TableArgs struct {
 	Table string
 	SQL   string
 }
 
+//定义"索引表命令"结构体类型
 type IndexArgs struct {
 	Index string
 	Table string
 	SQL   string
 }
 
+//在字符串数组中查找指定字符串的序号值
 func FindElement(pSlice *[]string, str string) int {
 	for i, elem := range *pSlice {
 		if elem == str {
@@ -49,12 +54,16 @@ func FindElement(pSlice *[]string, str string) int {
 	return -1
 }
 
+
+//向字符串数组中添加指定字符串（前提是字符串数组中没有该指定字符串）
 func AddUniqueToSlice(pSlice *[]string, str string) {
 	if FindElement(pSlice, str) == -1 {
 		*pSlice = append(*pSlice, str)
 	}
 }
 
+
+//从字符串数组中删除指定字符串
 func DeleteFromSlice(pSlice *[]string, str string) bool {
 	index := FindElement(pSlice, str)
 	if index == -1 {
@@ -65,11 +74,12 @@ func DeleteFromSlice(pSlice *[]string, str string) bool {
 	return true
 }
 
-// return self IP
+//返回自身的IP地址
 func GetHostIP() string {
 	return ""
 }
 
+//删除指定文件名的本地文件
 func DeleteLocalFile(fileName string) {
 	err := os.Remove(fileName)
 	if err != nil {
@@ -77,7 +87,7 @@ func DeleteLocalFile(fileName string) {
 	}
 }
 
-// rpc util
+//非阻塞的RPC超时处理函数
 func TimeoutRPC(call *rpc.Call, ms int) (*rpc.Call, error) {
 	select {
 	case res := <-call.Done:
@@ -87,7 +97,7 @@ func TimeoutRPC(call *rpc.Call, ms int) (*rpc.Call, error) {
 	}
 }
 
-// fs util
+//清理目录
 func CleanDir(localDir string) {
 	dir, err := ioutil.ReadDir(localDir)
 	if err != nil {
